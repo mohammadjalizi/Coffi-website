1fec03e53e6aa2b6dad1793cc4e01e3c38d0606a
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-29 17:24:24 +0330

    added photo

--- frontend/src/assets/APPbar/Navbar.jsx
+++ frontend/src/assets/APPbar/Navbar.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
+import img1 from '../imag/logo.png'
 import './Navbar.css'
 const Navbar = () => {
   return (
     <div className=' Navbar   text-white   '>
 
-
-<div className=' flex  gap-6 '>
-
+<div className='  flex   justify-between  py-5 '>
+<div className=' gap-6  '>  
 <a href=''>  صفحه ی اصلی </a>
-<a href=''>  صفحه ی اصلی </a>
+<a href=''>  در باره</a>
+<a href='#'>قهوه</a>
+</div>
+<div className=' flex justify-center items-center '>
+
+
+
+<img src={img1}/>
 </div>
 
-<div className=' header   h-72       '>
+</div>
 
-<h1 className=' text-xl '> روز خود را باقهوه شروع کنید  </h1>
+<div className='    h-72       '>
+  <div className=' header    '>
 
-<a href='' className='btn'>   خرید   </a>
+
+
+
+<h1 className='   text-2xl  my-10 '> روز خود را باقهوه شروع کنید  </h1>
+
+<a href='       ' className='btn     text-2xl   '>   خرید   </a>
+</div>
 
 
 </div>
