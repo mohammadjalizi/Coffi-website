a9ecf6779da38ed6ac913f827f8aea5049af9608
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-29 17:05:13 +0330

    added photo

---
+++ frontend/src/assets/APPbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className=' Navbar   text-white   '>
+
+
+<div className=' flex  gap-6 '>
+
+<a href=''>  صفحه ی اصلی </a>
+<a href=''>  صفحه ی اصلی </a>
+</div>
+
+<div className=' header   h-72       '>
+
+<h1 className=' text-xl '> روز خود را باقهوه شروع کنید  </h1>
+
+<a href='' className='btn'>   خرید   </a>
+
+
+</div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/assets/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './APPbar/Navbar'
+import Header from './Header/Header'
+
+const Root = () => {
+  return (
+    <>
+    
+
+<Navbar/>
+<Header/>
+</>
+  )
+}
+
+export default Root
