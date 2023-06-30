ad6d05ec806c787538d890bb51847f076f835877
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-06-30 17:45:43 +0330

    add file redux

---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+      id: 1,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 100,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1688058628/coffi/french-coffee-250med-768x768_f4mwqf.jpg",
+    },
+    {
+      id: 2,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 200,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1688058628/coffi/1-1_x1awcm.jpg",
+    },
+    {
+      id: 3,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 300,
+      imageLink: "https://res.cloudinary.com/dlz1h6dhr/image/upload/v1688058628/coffi/1-1_x1awcm.jpg",
+    },
+    {
+      id: 4,
+      productName: "T-shirt",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      price: 400,
+      imageLink: "",
+    },
+   ];
+   module.exports = products;
