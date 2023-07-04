08eb292baed52ee803fb6a55bc120e68a000ddd6
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-07-04 16:15:56 +0330

    add price

--- frontend/src/assets/Store/Store.jsx
+++ frontend/src/assets/Store/Store.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import './Store.css'
 import { useGetProductApiByNameQuery } from '../../Redux/ProductApi'
 const dataarry=[{},{},{}]
+import { BsFillBasketFill } from "react-icons/bs";
 const Store = () => {
   const { data, error, isLoading } = useGetProductApiByNameQuery()
 
@@ -31,6 +33,11 @@ return(
 
 <h1 className=' text-center my-2 '>{item.productName}</h1>
 <img src={item.imageLink}/>
+<div className='Icon'>  
+<h1  className='  h1 '>  {item.price} تومان </h1>
+<strong className='strong'>+</strong>
+    </div>
+
 
 
 
