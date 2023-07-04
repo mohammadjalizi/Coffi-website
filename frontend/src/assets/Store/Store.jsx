a8c545223779f2d61cd15b18b5c52288d3a8e6a4
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-07-04 14:50:43 +0330

    Add the service to your store

--- frontend/src/assets/Store/Store.jsx
+++ frontend/src/assets/Store/Store.jsx
@@ -1,25 +1,60 @@
 import React from 'react'
 import { useGetProductApiByNameQuery } from '../../Redux/ProductApi'
-
+const dataarry=[{},{},{}]
 const Store = () => {
-  const { data, error, isLoading } = useGetProductApiByNameQuery('')
+  const { data, error, isLoading } = useGetProductApiByNameQuery()
+
+  if (isLoading) {
+    return(
+<>
+erooorrr
+</>
+
+    )
+  }
+if (data) {
   return (
     <div>
-         {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>{data.species.name}</h3>
-          <img src={data.sprites.front_shiny} alt={data.species.name} />
-        </>
-      ) : null}
+
+
+      
 
 <h1 className='  text-[#b68834] text-3xl text-center md:my-8 '>فروشگاه قهوه</h1>
 
+<div className=' flex  flex-wrap '>   
+{
+data.map((item)=>{
+
+return(
+  
+<div key={item.id} className='  '>
+
+<h1>{item.productName}</h1>
+<img src={item.imageLink}/>
+
+
+
+</div>
+
+)
+
+
+})
+
+}
+</div>
     </div>
   )
+}
+if (error) {
+  return(
+
+<>
+erooor code
+</>
+
+  )
+}
 }
 
 export default Store
