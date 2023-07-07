80edfe6572899d28a43fbb1303e6e75ca21ae0d4
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-07-07 14:25:22 +0330

    Add  file seactiuon 2

--- frontend/src/assets/Seaction/Seaction.jsx
+++ frontend/src/assets/Seaction/Seaction.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import './Seaction.css'
+import img1 from '../imag/b1.jpg'
+import img3 from '../imag/b2.jpg'
 const Seaction = () => {
   return (
     <div>
@@ -15,7 +17,49 @@ const Seaction = () => {
             <p className=' text-center  my-5 '>   کسانی که به شدت عاشق سیستم سازگار با محیط زیست هستند.  </p>
             </div>
 
+            {/* photo */}
+<div className='  photo  flex justify-between flex-wrap'>
 
+
+
+<div className=''>
+
+<img src={img1}/>
+
+<ul className="post-tags">
+<li><a className=' hover:text-white ' href="#">سفر کردن</a></li>
+	<li><a className=' hover:text-white ' href="#">سبک زندگی</a></li>
+</ul>
+
+<h4>Portable latest Fashion for young women</h4>
+<p className='  w-full   md:w-96 '>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+</p>
+<p className="post-date">
+								31st January, 2018
+							</p>
+</div>
+
+<div>
+
+<img src={img1}/>
+
+<ul className="post-tags">
+<li><a className=' hover:text-white ' href="#">سفر کردن</a></li>
+	<li><a className=' hover:text-white ' href="#">سبک زندگی</a></li>
+</ul>
+
+<h4>Portable latest Fashion for young women</h4>
+<p className=' w-full   md:w-96'>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
+</p>
+<p className="post-date">
+								31st January, 2018
+							</p>
+</div>
+
+</div>
+{/* ende photo */}
     </div>
   )
 }
