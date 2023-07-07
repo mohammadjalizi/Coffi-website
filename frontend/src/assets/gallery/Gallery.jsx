5bdb0f32538af41c3d101ee7e28897757d5e50c6
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-07-07 13:46:36 +0330

    add store

--- frontend/src/assets/gallery/Gallery.jsx
+++ frontend/src/assets/gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Gallery.css'
-import bulkImages from './bulkImages'
+
 import image1 from './photo gallery/g1.jpg'
 import image2 from './photo gallery/g2.jpg'
 import image3 from './photo gallery/g3.jpg'
@@ -11,11 +11,11 @@ import image5 from './photo gallery/g5.jpg'
 const Gallery = () => {
   return (
     
-    <div className='   md:my-20  '>
+    <div className='  my-[78rem]  md:my-20  '>
         
         <div>         
             
-        <h1  className=' head  text-3xl text-center '>
+        <h1  className=' head   text-3xl text-center '>
 
 
         چه نوع قهوه ای برای شما سرو می کنیم
@@ -23,10 +23,10 @@ const Gallery = () => {
         </h1>
         <p className=' text-center  my-5 '>   کسانی که به شدت عاشق سیستم سازگار با محیط زیست هستند.  </p>
         </div>
-        <div className="  photo ">
+        <div className="      photo ">
 
     
-        <div className='row '>
+        <div className= '    row   '>
             <div className='column'>  
     <img src={image1}/>
     <img src={image2}/>
