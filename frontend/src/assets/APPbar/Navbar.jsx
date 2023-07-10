90d4b89fe1127b6b94fd57c63aed066ae76d8f99
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-07-10 17:55:44 +0330

    Add file navbar

--- frontend/src/assets/APPbar/Navbar.jsx
+++ frontend/src/assets/APPbar/Navbar.jsx
@@ -7,31 +7,44 @@
 
     const Navbar = () => {
     return(
-<div div className=' Navbar '>
+<div  className=' Navbar  '>
 <div className='  flex justify-between  '>
-<div>
+<div className=' mt-3 '>
 <img  className=' hidden md:block w-12  '  src={img1}/>
 
 
 </div>
-<div>  
+<div className=' mt-3 '>  
 
 
 
-<a  className=' list-none mr-3 '  href='#'> صفحه ی اصلی </a>
-<a  className=' list-none mr-3 '  href='#'>  قهوه  </a>
-<a  className=' list-none mr-3 '  href='#'>  سبد خرید  </a>
-<a   className=' list-none mr-3 ' href='#'>  درباره ی ما  </a>
+<a  className='   text-white  list-none mr-3 '  href='#'> صفحه ی اصلی </a>
+<a  className='  text-white  list-none mr-3 '  href='#'>  قهوه  </a>
+<a  className='   text-white list-none mr-3 '  href='#'>  سبد خرید  </a>
+<a   className='  text-white  list-none mr-3 ' href='#'>  درباره ی ما  </a>
 
 
 </div>
-<div className=' flex items-center  '>
+<div className='  mt-3 flex items-center  '>
 <span className=' relative  cart__icon'>
 
 <  PiShoppingCartSimpleLight className=' text-white text-2xl '/>
 <span className="cart__badge">0</span>
 </span>
 
+
+</div>
+</div>
+
+<div className=' relative header '>
+<div className=' absolute left-3  top-32'>    
+<h1 className=' text-white   my-10  '> اکنون می توانید انرژی را احساس کنید  </h1>
+
+<p className='  heading text-white  '> روز خود را با
+یک قهوه شروع کنید </p>
+<a href="#" className="  my-4 text-center my-5 primary-btn text-uppercase">
+خرید قهوه
+</a>
 
 </div>
 </div>
