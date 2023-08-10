7d97ab3e60a05b7d6747f3362074298d3333a29e
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-08-10 17:38:58 +0330

    Add cart product

--- frontend/src/assets/cart/Cart.jsx
+++ frontend/src/assets/cart/Cart.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Navbar from '../APPbar/Navbar'
 import { Footer } from '../footer/Footer'
-
+import { useSelector } from 'react-redux'
+import './Cart.css'
 const Cart = () => {
-
+  const {selectedProducts} = useSelector((state) => state.carttt)
   return (
  
     
     <div className='  '>
- 
-        
-cartttt
+ {selectedProducts.map((item)=>{
 
+return (
+
+<div>
+
+<>
+ 
+</>
+
+
+  </div>
+
+)
+
+ })}
+  
     </div>
   
   )
