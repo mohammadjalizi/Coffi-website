e0ac7b696be99945ce0ecd703aff6297897b06e2
mjalizi1376@gmail.com <mjalizi1376@gmail.com>
2023-09-21 19:31:40 +0330

    Add footer

--- frontend/src/assets/cart/Cart.jsx
+++ frontend/src/assets/cart/Cart.jsx
@@ -3,6 +3,8 @@ import Navbar from '../APPbar/Navbar'
 import { Footer } from '../footer/Footer'
 import { useSelector } from 'react-redux'
 import './Cart.css'
+import { PiShoppingCartSimpleLight } from 'react-icons/pi'
+
 const Cart = () => {
   const {selectedProducts} = useSelector((state) => state.carttt)
   return (
@@ -11,13 +13,36 @@ const Cart = () => {
     <div className='  '>
  {selectedProducts.map((item)=>{
 
+ 
+
 return (
 
 <div>
 
-<>
- 
-</>
+<div key={item.id} className=' p-[30px]  shadow-2xl Card  w-80  hover:shadow-none    '>
+
+
+<img src={item.imageLink}/>
+<div className='  '>
+<h1 className=' text-center my-2 text-xs '>{item.productName}</h1>
+<div className='  flex justify-between items-center my-2  '>  
+<h1  className='  h1 text-xl '>  {item.price} تومان </h1>
+<strong  onClick={()=>{
+
+
+}} className='  cursor-pointer bg-orange-600  text-sm  '>  <  PiShoppingCartSimpleLight onClick={()=>{
+
+  dispatch(addToCart(item))
+
+}} className='  text-2xl '/>   </strong>
+    </div>
+  </div>
+
+
+
+
+
+</div>
 
 
   </div>
